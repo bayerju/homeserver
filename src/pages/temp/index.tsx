eb3f068f8eea94cb65d596786e6aebc06a34b3d5
bayerju <bayerju@users.noreply.github.com>
2022-11-30 20:42:35 +0100

    kitchen works login in progress

--- src/pages/temp/index.tsx
+++ src/pages/temp/index.tsx
@@ -1,12 +1,38 @@
 import { useSession } from 'next-auth/react';
 import { trpc } from '../../utils/trpc';
 import map from 'lodash/map';
+// import css modules
+import styles from './style.module.scss';
 
-const SensorComponent = (sensorData: any) => {
+interface SensorData {
+    room: string;
+    temperature: number;
+    humidity: number;
+}
+interface PropTypes {
+    sensorData: SensorData;
+}
+const SensorComponent = (props: PropTypes) => {
+    console.log(props.sensorData);
     return (
         <div>
             <h1 className="">Sensor</h1>
-            <div>{JSON.stringify(sensorData)}</div>
+            <table className={styles.sensortable}>
+                <thead>
+                    <tr>
+                        <th>Raum</th>
+                        <th>Temperatur</th>
+                        <th>Luftfeuchtigkeit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{props.sensorData.room}</td>
+                        <td>{props.sensorData.temperature}</td>
+                        <td>{Math.round(props.sensorData.humidity)}</td>
+                    </tr>
+                </tbody>
+            </table>
 
         </div>
     );
@@ -19,17 +45,17 @@ const Temp = () => {
     const tempData = trpc.temp.getTemps.useQuery();
     const tempDataSecret = session.status === 'authenticated' ? trpc.temp.getSecretTemps.useQuery() : trpc.temp.getTemps.useQuery();
 
-    const data = {
-        ...tempData.data,
-        ...tempDataSecret.data
+    const data: SensorData[] = []
+    if (tempData.data) {
+        data.push(tempData.data);
     }
     return (
         <div>
             <h1 className="">Temp</h1>
             <div className='flex '>
-                {map(data, (sensorData: any) => {
+                {tempData.isLoading || tempDataSecret.isLoading ? <div>Loading...</div> : map(data, (sensorData) => {
                     return (
-                        <SensorComponent {...sensorData} />
+                        <SensorComponent sensorData={sensorData} />
                     )
                 })}
             </div>
--- src/server/trpc/router/temp.ts
+++ src/server/trpc/router/temp.ts
@@ -1,34 +1,53 @@
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 import { z } from "zod";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const sensors = {
     raspZero: {
         id: 1,
         location: "kitchen",
-        ip: "192.168.178.37"
+        url: "http://raspberrypizero:3002/temp"
     },
     RaspPi4: {
         id: 0,
         location: "julian",
-        ip: "192.168.178.32"
+        url: "http://raspberrypi:3002/temp" // "192.168.178.32"
     }
 }
 
 const sensorPorts = "3002"
 
+const options = {
+    method: 'GET',
+    url: 'http://raspberrypi:3002/temp',
+    headers: { Accept: '*/*', 'User-Agent': 'Thunder Client (https://www.thunderclient.com)' }
+};
+
+interface ResponseData {
+    response: {
+        temperature: number,
+        humidity: number,
+    }
+}
+
 export const tempRouter = router({
     getTemps: publicProcedure.query(async ({ ctx }) => {
-        // const kitchenData = await axios.get(`${sensors.raspZero.ip}:${sensorPorts}/temp`);
-
+        const kitchenData: AxiosResponse<ResponseData> = await axios.get(sensors.raspZero.url);
+        console.log(kitchenData.data);
         // //get current weather in Kiel
         // const currentWeather = await axios.get("https://api.openweathermap.org/data/2.5/weather?q=Kiel&appid=2b3e1b1b1b1b1b1b1b1b1b1b1b1b1b1b&units=metric")
-        // return { kitchen: kitchenData.data, currentWeather: currentWeather.data };
-        return { kitchen: 20, currentWeather: 20 };
+        return { room: "kitchen", temperature: kitchenData.data.response.temperature, humidity: kitchenData.data.response.humidity };
+        // return { kitchen: 20, currentWeather: 20 };
     }),
     getSecretTemps: protectedProcedure
         .query(async ({ ctx }) => {
-            const julianData = await axios.get(`${sensors.RaspPi4.ip}:${sensorPorts}/temp`);
-            return { julian: julianData.data };
+            axios.request(options).then(function (response) {
+                console.log(response.data);
+            }).catch(function (error) {
+                console.error(error);
+            });
+            //     const julianData = await axios.get(`${sensors.RaspPi4.ip}:${sensorPorts}/temp`);
+            //     return { julian: julianData.data };
+
         }),
 });
